99a85a7fbac4222999cb2618b7bcbcb60b369a6e
Gustavo Parizzato <86897255+gustavoparizzato@users.noreply.github.com>
2022-07-15 17:17:01 -0300

    Changed formatting in sendSms 2

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -1,7 +1,5 @@
 package com.devsuperior.dsmeta.services;
 
-import java.text.DecimalFormat;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -35,8 +33,8 @@ public class SmsService {
 		Sale sale = saleRepository.findById(saleId).get();
 		
 		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
-		String msg = "O Vendedor " + sale.getSellerName() + " foi destaque em " + date 
-				+ " com um total de R$ " + new DecimalFormat("#,##0.00").format(sale.getAmount()) + " em Vendas ";
+		String msg = String.format("O vendedor %s foi destaque em %s com um total de R$ %.2f", 
+				sale.getSellerName(), date, sale.getAmount()) + " em Vendas ";
 
 		Twilio.init(twilioSid, twilioKey);
 
